475cad6f16c394faca42b964df20cb276696d200
chowdhury-eshita <chowdhuryeshita58@gmail.com>
2022-01-12 18:52:06 +0600

    update

--- index.js
+++ index.js
@@ -76,13 +76,13 @@ async function run() {
             res.json(result);
         });
 
-        // //Delete API
-        // app.delete('/myOrders/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await myOrdersCollection.deleteOne(query);
-        //     res.json(result);
-        // })
+        //Delete API
+        app.delete('/myOrders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await myOrdersCollection.deleteOne(query);
+            res.json(result);
+        })
 
     }
     finally {
